fd0edc69370cb1cac083cecd1c7c89a067390f8b
Aakash Raghunathan <aakshu.goku@gmail.com>
2019-09-26 17:02:02 +0530

    First Commit

---
+++ app/__init__.py
@@ -0,0 +1,51 @@
+#init file for app package
+from flask import Flask
+from config import Config
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
+from flask_login import LoginManager
+import logging
+from logging.handlers import SMTPHandler, RotatingFileHandler
+import os
+from flask_mail import Mail
+from flask_bootstrap import Bootstrap
+
+my_app = Flask(__name__)
+bootstrap = Bootstrap(my_app)
+my_app.config.from_object(Config)
+db = SQLAlchemy(my_app)
+mail = Mail(my_app)
+migrate = Migrate(my_app, db)
+login = LoginManager(my_app)
+login.login_view = 'login'
+#my_app.config['SECRET_KEY'] = 'kiss-my-ass'
+
+from app import routes, models, errors
+
+if not my_app.debug:
+    if my_app.config['MAIL_SERVER']:
+        auth = None
+        if my_app.config['MAIL_USERNAME'] or my_app.config['MAIL_PASSWORD']:
+            auth = (my_app.config['MAIL_USERNAME'], my_app.config['MAIL_PASSWORD'])
+        secure = None
+        if my_app.config['MAIL_USE_TLS']:
+            secure = ()
+        mail_handler = SMTPHandler(
+            mailhost=(my_app.config['MAIL_SERVER'], my_app.config['MAIL_PORT']),
+            fromaddr='no-reply@' + my_app.config['MAIL_SERVER'],
+            toaddrs=my_app.config['ADMINS'], subject='Microblog Failure',
+            credentials=auth, secure=secure)
+        mail_handler.setLevel(logging.ERROR)
+        my_app.logger.addHandler(mail_handler)
+
+    if not os.path.exists('logs'):
+        os.mkdir('logs')
+    file_handler = RotatingFileHandler('logs/microblog.log', maxBytes=10240,
+                                       backupCount=10)
+    file_handler.setFormatter(logging.Formatter(
+        '%(asctime)s %(levelname)s: %(message)s [in %(pathname)s:%(lineno)d]'))
+    file_handler.setLevel(logging.INFO)
+    my_app.logger.addHandler(file_handler)
+
+    my_app.logger.setLevel(logging.INFO)
+    my_app.logger.info('Microblog startup')
